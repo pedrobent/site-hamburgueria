1b9767c7e778e6c211386d2c4abfd871d2223b5f
pedro <pedrobento131@gmail.com>
2025-10-22 18:40:16 -0300

    static

--- pedidos/settings.py
+++ pedidos/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
-# Configurações de Mídia
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
