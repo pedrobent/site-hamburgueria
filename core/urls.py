fe8baf4d0033a1f37b5e4894c3defd58523b9228
pedro <pedrobento131@gmail.com>
2025-10-28 15:37:47 -0300

    cadastro

--- core/urls.py
+++ core/urls.py
@@ -11,11 +11,11 @@ router.register(r'pedidos', PedidoViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('', lista_produtos, name='lista_produtos'),
+    path('lista de produtos/', lista_produtos, name='lista_produtos'),
     path('carrinho/', ver_carrinho, name='ver_carrinho'),
     path('carrinho/adicionar/<int:produto_id>/', adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
     path('carrinho/finalizar/', finalizar_pedido, name='finalizar_pedido'),
     path('pedidos/', pedido_list, name='pedido_list'),
     path('criar-pedido/', criar_pedido, name='criar_pedido'),
-    path('cadastro-cliente/', cadastro_cliente, name='cadastro_cliente'),
+    path('', cadastro_cliente, name='cadastro_cliente'),
 ] 
